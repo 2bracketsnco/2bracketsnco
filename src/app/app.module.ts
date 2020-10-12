15253f236301ad047b2c42bc443ae13e6cccc10e
KaushalShah1307 <KaushalShah1307@gmail.com>
2020-10-12 12:00:10 +0530

    feat: bulk file upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DekComponent } from './dek/dek.component';
+import { PreloaderComponent } from './preloader/preloader.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DekComponent,
+    PreloaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
