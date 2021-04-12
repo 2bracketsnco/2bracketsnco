f68d506c808a071073a350e6ada1be44db0179df
KaushalShah1307 <KaushalShah1307@gmail.com>
2021-04-12 14:45:39 +0530

    New look for 2bnc website

---
+++ README.md
@@ -0,0 +1,1 @@
+# 2 Brackets & Co.
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
+
+import { IndexComponent } from "./pages/index/index.component";
+import { RegisterpageComponent } from "./pages/aboutus/aboutus.component";
+import { LandingpageComponent } from "./pages/services/services.component";
+
+const routes: Routes = [
+  // { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "", component: IndexComponent },
+  { path: "aboutus", component: RegisterpageComponent },
+  { path: "ourservices", component: LandingpageComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      // useHash: true
+    })
+  ],
+  exports: []
+})
+export class AppRoutingModule {}
